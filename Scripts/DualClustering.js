d0d031fd080b735b79d50d9adb5a9327191a70dd
James Li <jamesxli@visumap.com>
2021-07-26 15:03:43 -0600

    Initial check-in

--- Scripts/DualClustering.js
+++ Scripts/DualClustering.js
@@ -1,27 +1,36 @@
 //DualClustering.js
 //
-//Description: Cluster the rows and columns of a number table of the 
+//Cluster the rows and columns of a number table of the 
 //parent data view.
 //
-//======================================================================
+/*
+To install this script, select and run the following line:
+
+vv.GuiManager.SetCustomMenu('Dual Clustering', True, vv.CurrentScriptPath, 'HeatMap')
+
+To remove this context menu, select and run the following line:
+
+vv.GuiManager.RemoveCustomMenu('Dual Clustering')
+*/
+
 var nt = pp.GetNumberTable().Clone();
 var mds = New.MdsCluster(nt);
 mds.Show();
 mds.Metric="Correlation.Cosine Distance";
 mds.ClusterAlgorithm = 4;
 mds.AutoClustering = false;
-mds.TheForm.HdClusterNoise = true;
+mds.TheForm.HdbClusterNoise = true;
 
-[mds.TheForm.MinPoints, mds.TheForm.MinClusterSize ] = [5, 50];
+[mds.TheForm.HdbMinPoints, mds.TheForm.HdbMinClusterSize ] = [5, 50];
 mds.Reset().Start().ClusterData();
 
-//mds.Show2DView();
+var rowMap = mds.Show2DView();
 for(var i=0; i<mds.BodyList.Count; i++)
 	nt.RowSpecList[i].Type = mds.BodyList[i].Type;
 var rowClusters = mds.TheForm.ClustersFound;
 
 mds.SetTrainingData(nt.Transpose2());
-[mds.TheForm.MinPoints, mds.TheForm.MinClusterSize ] = [3, 30];
+[mds.TheForm.HdbMinPoints, mds.TheForm.HdbMinClusterSize ] = [3, 30];
 mds.Reset().Start().ClusterData();
 
 for(var i=0; i<mds.BodyList.Count; i++)
@@ -29,6 +38,11 @@ for(var i=0; i<mds.BodyList.Count; i++)
 var colClusters = mds.TheForm.ClustersFound;
 var hm = nt.ShowHeatMap();
 hm.Title = "Row/Column Clusters: " + rowClusters + "/" + colClusters;
+var colMap = mds.Show2DView();
 mds.Close();
 
+var sz = 320;
+hm.TheForm.SetBounds(600, 500, 800, 540);
+rowMap.TheForm.SetBounds(615 - sz, 650, sz, sz);
+colMap.TheForm.SetBounds(800, 508-sz, sz, sz);
 
