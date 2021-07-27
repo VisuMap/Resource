42f06d1191d73a6580ae1a643a4fc8951afb814d
James Li <jamesxli@visumap.com>
2021-07-26 19:21:18 -0600

    Added 3D support

--- Scripts/DualClustering.js
+++ Scripts/DualClustering.js
@@ -16,21 +16,27 @@ vv.GuiManager.RemoveCustomMenu('Dual Clustering')
 var nt = pp.GetNumberTable().Clone();
 var mds = New.MdsCluster(nt);
 mds.Show();
+mds.Is3D = false;
 mds.Metric="Correlation.Cosine Distance";
 mds.ClusterAlgorithm = 4;
 mds.AutoClustering = false;
-mds.TheForm.HdbClusterNoise = true;
+mds.AutoNormalizing = false;
+var frm = mds.TheForm;
+frm.TsneExaFactor = 12;
+frm.TsneExaSmoothen = true;
+frm.TsneMaxLoops = 5000;
+frm.HdbClusterNoise = true;
 
-[mds.TheForm.HdbMinPoints, mds.TheForm.HdbMinClusterSize ] = [5, 50];
+[frm.HdbMinPoints, frm.HdbMinClusterSize ] = [5, 50];
 mds.Reset().Start().ClusterData();
 
-var rowMap = mds.Show2DView();
+var rowMap = mds.Is3D ? mds.Show3DView() : mds.Show2DView();
 for(var i=0; i<mds.BodyList.Count; i++)
 	nt.RowSpecList[i].Type = mds.BodyList[i].Type;
 var rowClusters = mds.TheForm.ClustersFound;
 
 mds.SetTrainingData(nt.Transpose2());
-[mds.TheForm.HdbMinPoints, mds.TheForm.HdbMinClusterSize ] = [3, 30];
+[frm.HdbMinPoints, frm.HdbMinClusterSize ] = [3, 30];
 mds.Reset().Start().ClusterData();
 
 for(var i=0; i<mds.BodyList.Count; i++)
@@ -38,10 +44,12 @@ for(var i=0; i<mds.BodyList.Count; i++)
 var colClusters = mds.TheForm.ClustersFound;
 var hm = nt.ShowHeatMap();
 hm.Title = "Row/Column Clusters: " + rowClusters + "/" + colClusters;
-var colMap = mds.Show2DView();
+var colMap = mds.Is3D ? mds.Show3DView() : mds.Show2DView();
 mds.Close();
 
 var sz = 320;
+rowMap.NormalizeView();
+colMap.NormalizeView();
 hm.TheForm.SetBounds(600, 500, 800, 540);
 rowMap.TheForm.SetBounds(615 - sz, 650, sz, sz);
 colMap.TheForm.SetBounds(800, 508-sz, sz, sz);
